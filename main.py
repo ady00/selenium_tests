1d475cb4532769f6f40e00778569f9a8fb7cbda7
Ady <advayb2018@gmail.com>
2022-11-21 16:25:16 -0800

    finalized selenium for spotify

--- main.py
+++ main.py
@@ -14,9 +14,34 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 url = "https://qvoter.vercel.app/election?election=ef9a120b-07fe-4aff-99a4-701af84ccfae"
 driver.get(url)
-time.sleep(3) #if you want to wait 3 seconds for the page to load
+time.sleep(3) # incrementable
 page = driver.page_source
 driver.quit()
 soup = BeautifulSoup(page, 'html.parser')
 
-print(soup.prettify())
+temp_song = soup.find_all("div", {"class": "col-8"})
+
+song_array = []
+
+for song in temp_song:
+    song_array.append(song.decode_contents().strip()) 
+
+song_array.pop(0)
+print(song_array)
+
+
+
+temp_votes = soup.find_all("div", {"class": "col-4"})
+
+vote_array = []
+
+for song in temp_votes:
+    vote_array.append(song.decode_contents().strip()) 
+
+song_array.pop(0) 
+print(song_array)
+
+
+
+
+
